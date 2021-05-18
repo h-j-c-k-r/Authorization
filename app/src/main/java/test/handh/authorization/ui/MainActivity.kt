12bf3adf8534ac9ec545b6bc2537e041606cb5a9
Hijacker <v.kuzmin@profsoft.pro>
2021-05-19 00:14:27 +0400

    layout, styles

--- app/src/main/java/test/handh/authorization/ui/MainActivity.kt
+++ app/src/main/java/test/handh/authorization/ui/MainActivity.kt
@@ -1,17 +1,12 @@
 package test.handh.authorization.ui
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import test.handh.authorization.R
 import test.handh.authorization.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding by viewBinding(ActivityMainBinding::bind)
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
 }
--- app/src/main/java/test/handh/authorization/ui/authorization/AuthorizationFragment.kt
+++ app/src/main/java/test/handh/authorization/ui/authorization/AuthorizationFragment.kt
@@ -1,7 +1,17 @@
 package test.handh.authorization.ui.authorization
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import test.handh.authorization.R
+import test.handh.authorization.databinding.FragmentAuthorizationBinding
 
-class AuthorizationFragment: Fragment() {
+class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
+    private val binding: FragmentAuthorizationBinding by viewBinding(FragmentAuthorizationBinding::bind)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
